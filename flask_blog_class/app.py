05e6d79cb9a82b181793fc52f946ace02dbc5acf
MazinKing <72046918+Keylith2020@users.noreply.github.com>
2024-10-31 22:46:24 -0500

    Added the edit functionality

--- flask_blog_class/app.py
+++ flask_blog_class/app.py
@@ -20,6 +20,16 @@ def get_db_connection():
     #return the connection object
     return conn
 
+#Function to retrieve a post from the database
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if post is None:
+        abort(404)
+
+    return post
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
@@ -63,6 +73,36 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+#create route to edit post
+#pass the post id as url parameter
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+    #determine get or post
+
+    if request.method == 'POST':
+        #get title and content
+        title = request.form['title']
+        content = request.form['content']
+
+        #if not title or content, flash an error message
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+            conn.commit()
+            conn.close()
+
+            #redirect to home page
+            return redirect(url_for('index'))
+        
+  
+
+#If get then display page
+    return render_template('edit.html', post=post)
 
 
 
